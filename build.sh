d7cf4952c2e7ce798bc56bbc3f7e7c18c0c2fcbd
AtomHare <29772841+AtomHare@users.noreply.github.com>
2025-04-30 21:27:40 +0200

    Add devcontainer

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+mkdir -p love
+dotnet restore sharp/*.csproj
+dotnet publish sharp/*.csproj --runtime linux-x64
+
+cp -rfv sharp/bin/Release/net8.0/linux-x64/publish love/sharp
+
+cd love 
+curl -O https://github.com/love2d/love/releases/download/11.5/love-11.5-x86_64.AppImage
+cd ..
+cp -a luarocks/lib/lua/5.1/. love/
+
+cd src 
+zip -r ../love/olympus.love *
---
+++ prepare-dev-env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# =======================================================
+sudo add-apt-repository ppa:bartbes/love-stable -y
+sudo apt-get update
+sudo apt-get install love luarocks libgtk-3-dev -y
+
+# not needed
+# luarocks config lua_version 5.1
+# luarocks
+
+# required for luarocks install
+git config --global url."https://github.com/".insteadOf git://github.com/
+
+luarocks $LUAROCKSPREARGS install --tree=luarocks https://raw.githubusercontent.com/maddie480/lua-subprocess/master/subprocess-scm-1.rockspec $LUAROCKSARGS
+luarocks $LUAROCKSPREARGS install --tree=luarocks https://raw.githubusercontent.com/Vexatos/nativefiledialog/master/lua/nfd-scm-1.rockspec $LUAROCKSARGS
+luarocks $LUAROCKSPREARGS install --tree=luarocks lsqlite3complete $LUAROCKSARGS
+
+# =======================================================
+# build
